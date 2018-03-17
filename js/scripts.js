b1c530427cca17fe576075372bf2c9a0d3d5d3a5
Joanna Fengler <asiapobuta@wp.pl>
2018-03-17 15:49:21 +0100

    tables

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+allNames.splice(1, 0, 'Adaś');
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+	if (allNames.indexOf(newName) === -1) {
+		var x = allNames.push('Marian');
+	}
+
+console.log(allNames);
